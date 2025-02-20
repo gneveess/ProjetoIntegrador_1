ace6c1dad6a4da534b4e0ec9e53f85203314b13d
gneveess <gabriel.nf3@puccampinas.edu.br>
2025-02-20 14:17:14 -0300

    Leiam este aviso pessoalgit add .

---
+++ leiam_este_aviso.py
@@ -0,0 +1,4 @@
+#Pessoal, peço que todos vocês baixem a IDE para codar em python, que se chama Microsoft visual code
+#tambem instalem python em suas maquinas. diretamente do google mesmo.
+
+print("Valeu!")
