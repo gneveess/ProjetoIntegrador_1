1217de8f457f400be98f3448871a2bccb5992989
gneveess <gabriel.nf3@puccampinas.edu.br>
2025-02-21 19:13:23 -0300

    apenas testando

---
+++ teste.py
@@ -0,0 +1,9 @@
+#Apenas um teste de desempenho da minha maquina
+
+print("Olá, como vc se chama?")
+nomecliente = input()
+
+print("e agora sua idade: ")
+idadecliente = input()
+
+print(nomecliente, idadecliente)
